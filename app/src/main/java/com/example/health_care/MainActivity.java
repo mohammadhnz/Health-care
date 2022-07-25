69a2f7f4fc5f5395ab75a088f17edf16b134d1a8
royaghavami <59202308+royaghavami@users.noreply.github.com>
2022-07-25 18:22:11 +0430

    fit: customer main page UI

--- app/src/main/java/com/example/health_care/MainActivity.java
+++ app/src/main/java/com/example/health_care/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.customer_main_page);
     }
 }
