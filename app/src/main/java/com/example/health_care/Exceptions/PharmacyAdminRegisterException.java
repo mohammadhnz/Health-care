5c31f361b0b9f83fa42fd0544c9bb79d04f4f990
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2022-07-25 17:08:56 +0430

    feat(pharmacy): add register panel and models

---
+++ app/src/main/java/com/example/health_care/Exceptions/PharmacyAdminRegisterException.java
@@ -0,0 +1,7 @@
+package com.example.health_care.Exceptions;
+
+public class PharmacyAdminRegisterException extends Exception{
+    public PharmacyAdminRegisterException(String message) {
+        super(message);
+    }
+}
