85b5221e7c65ceb258067f38b5e417610a16e104
royaghavami <59202308+royaghavami@users.noreply.github.com>
2022-07-27 18:39:47 +0430

    fit: pharmacy search and live updating

--- app/src/main/java/com/example/health_care/activities/DrugPage.java
+++ app/src/main/java/com/example/health_care/activities/DrugPage.java
@@ -1,13 +1,17 @@
 package com.example.health_care.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.health_care.R;
 import com.example.health_care.models.Drug;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 import java.util.Objects;
 
@@ -15,8 +19,12 @@ public class DrugPage extends AppCompatActivity {
 
     EditText drugNameT;
     EditText drugPriceT;
+    EditText drugDescription;
+    SearchView searchView;
     ImageView drugImage;
+    RecyclerView pharmaciesRecycler;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +32,46 @@ public class DrugPage extends AppCompatActivity {
 
         drugNameT = findViewById(R.id.drug_page_name_id);
         drugPriceT = findViewById(R.id.drug_page_price);
-        drugImage = findViewById(R.id.drug_img_recId);
+        drugImage = findViewById(R.id.drug_img_d_recId);
+        drugDescription = findViewById(R.id.drug_des_d_id);
+        searchView = findViewById(R.id.pharmacy_drug_page_id);
+        pharmaciesRecycler = findViewById(R.id.search_result_drug_page);
+
 
         Intent intent = getIntent();
         String drugId = intent.getStringExtra("drugId");
-        String drugName = Objects.requireNonNull(Drug.findDrugById(drugId)).getName();
+        Drug drug = Drug.findDrugById(drugId);
+        assert drug != null;
+        drugNameT.setText(drug.getName());
+        drugPriceT.setText("price: " + String.valueOf(drug.getPrice()));
+        drugDescription.setText(drug.getDescription());
+        if (drug.getPhoto().isEmpty()){
+            int drugImg = getResources().getIdentifier("pngitem_479890", "drawable", this.getPackageName());
+            drugImage.setImageResource(drugImg);
+        }
 
-        drugNameT.setText(drugName);
+        searchView.setIconified(false);
+        searchView.setIconifiedByDefault(false);
+
+        androidx.appcompat.widget.SearchView.SearchAutoComplete searchAutoComplete = (androidx.appcompat.widget.SearchView.SearchAutoComplete)searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        searchAutoComplete.setHintTextColor(getResources().getColor(R.color.purple_200));
+        searchAutoComplete.setTextColor(getResources().getColor(R.color.purple_700));
+
+//        searchView.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                filter(newText);
+//                return false;
+//            }
+//        });
+
     }
+
+
+
 }
